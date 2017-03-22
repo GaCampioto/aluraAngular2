ea289e3e8be22fdf3d679c6fa904732ea5ac312e
GaCampioto <gcampioto@gmail.com>
2017-03-21 23:04:07 -0300

    Consumindo serviço e ngFor

--- client/app/app.module.js
+++ client/app/app.module.js
@@ -10,6 +10,9 @@ var core_1 = require("@angular/core");
 var platform_browser_1 = require("@angular/platform-browser");
 var app_component_1 = require("./app.component");
 var photo_module_1 = require("./photo/photo.module");
+var http_1 = require("@angular/http");
+//Import diferente porque não é padrão do angular
+require("rxjs/add/operator/map");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -17,7 +20,7 @@ var AppModule = (function () {
 }());
 AppModule = __decorate([
     core_1.NgModule({
-        imports: [platform_browser_1.BrowserModule, photo_module_1.PhotoModule],
+        imports: [platform_browser_1.BrowserModule, photo_module_1.PhotoModule, http_1.HttpModule],
         declarations: [app_component_1.AppComponent],
         bootstrap: [app_component_1.AppComponent]
     })
--- client/app/app.module.ts
+++ client/app/app.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PhotoModule } from './photo/photo.module';
+import { HttpModule } from '@angular/http';
+//Import diferente porque não é padrão do angular
+import 'rxjs/add/operator/map';
 
 @NgModule({
-    imports : [BrowserModule, PhotoModule],
+    imports : [BrowserModule, PhotoModule, HttpModule],
     declarations : [AppComponent],
     bootstrap : [ AppComponent ]
 })
